e97e7777cf01cb35c9cbd71f6108516db9e91cfb
Mike Tashker <tashkerm@transdecsys.com>
2015-10-22 13:53:52 -0700

    PathEdit 1.1

---
+++ DirectoryBrowser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace PathEdit
+{
+    public class DirectoryBrowser
+    {
+        private static FolderBrowserDialog dlg;
+
+        public static string GetFolderFromUser()
+        {
+            using (dlg = new FolderBrowserDialog())
+            {
+                dlg.Description = "Browse for the new path.";
+                dlg.RootFolder = Environment.SpecialFolder.MyComputer;
+                dlg.SelectedPath = Path.GetPathRoot(Environment.GetFolderPath(Environment.SpecialFolder.System));
+                DialogResult dialogResult = dlg.ShowDialog();
+                return dialogResult == DialogResult.OK ? dlg.SelectedPath : null;
+            }
+        }
+    }
+}
