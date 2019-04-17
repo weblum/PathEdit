d2bcca21b4192d15ba2922ea7e316a33d0fb59d7
tashkerm <36169619+tashkerm@users.noreply.github.com>
2019-04-17 11:10:46 -0700

    Prepare startup for command line parameters; skeleton EditItem class

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using System.Windows;
+using System.IO;
 
 namespace PathEdit
 {
@@ -13,5 +9,37 @@ namespace PathEdit
 	/// </summary>
 	public partial class App : Application
 	{
-	}
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if (e.Args.Length > 0)
+            {
+                List<EditItem> EditItemList;
+                if (e.Args.Length == 1 && File.Exists(e.Args[0]))
+                    EditItemList = ProcessScriptFile(e.Args[0]);
+                else
+                    EditItemList = ProcessCommandLine(e.Args);
+                ProcessActions(EditItemList);
+            }
+            else
+                new MainWindow().Show();
+        }
+
+        private List<EditItem> ProcessScriptFile(string FileName)
+        {
+            return null;
+        }
+
+        private List<EditItem> ProcessCommandLine(string[] args)
+        {
+            return null;
+        }
+
+        void ProcessActions(List<EditItem> EditItemList)
+        {
+            foreach (var edit in EditItemList)
+            {
+                edit.Execute();
+            }
+        }
+    }
 }
---
+++ EditItem.cs
@@ -0,0 +1,34 @@
+﻿namespace PathEdit
+{
+    public class EditItem
+    {
+        enum eAction
+        {
+            Add,
+            Delete
+        }
+
+        enum eHive
+        {
+            NotSpecified,
+            User,
+            Global
+        }
+
+        enum eLocation
+        {
+            Front,
+            Back
+        }
+
+        private string PathString { get; set; }
+        private eAction Action { get; set; }
+        private eHive Hive { get; set; }
+        private eLocation Location { get; set; }
+
+        public bool Execute()
+        {
+            return true;
+        }
+    }
+}
