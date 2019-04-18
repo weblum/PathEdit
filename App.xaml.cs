c03c024fab40e1b67393378f08b726372f9dbae3
William E. Blum <wblum@pacbell.net>
2019-04-17 20:28:41 -0700

    Add some code to flesh out the alternate startup.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,43 +1,79 @@
-﻿using System.Collections.Generic;
+﻿//---------------------------------------------------------------------------
+// FILE NAME: App.xaml.cs
+// DATE:      Wednesday, April 17, 2019   8 pm
+// WEATHER:   Fair, Temp 63°F, Pressure 30.13",
+//            Humidity 56%, Wind 8.1 mph from the West
+// Programmer's Cuvee XLV
+// Copyright (C) 2019 William E. Blum.  All rights reserved.
+//---------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.IO;
 
 namespace PathEdit
 {
-	/// <summary>
-	/// Interaction logic for App.xaml
-	/// </summary>
-	public partial class App : Application
+	public partial class App //  : Application
 	{
-        private void Application_Startup(object sender, StartupEventArgs e)
+		private const string Title = "Path Editor";
+
+		#region Overrides of Application
+
+		protected override void OnStartup(StartupEventArgs e)
+		{
+			if (e.Args.Length > 0)
+				DoCommandsAndExit(e);
+			else
+				new MainWindow().Show();
+
+
+			base.OnStartup(e);
+		}
+
+		#endregion
+
+		private void DoCommandsAndExit(StartupEventArgs e)
+		{
+			List<EditItem> EditItemList;
+			if (e.Args.Length == 1 && File.Exists(e.Args[0]))
+				EditItemList = ProcessScriptFile(e.Args[0]);
+			else
+				EditItemList = ProcessCommandLine(e.Args);
+			ProcessActions(EditItemList);
+
+			Shutdown();
+		}
+
+		private static List<EditItem> ProcessScriptFile(string FileName)
         {
-            if (e.Args.Length > 0)
-            {
-                List<EditItem> EditItemList;
-                if (e.Args.Length == 1 && File.Exists(e.Args[0]))
-                    EditItemList = ProcessScriptFile(e.Args[0]);
-                else
-                    EditItemList = ProcessCommandLine(e.Args);
-                ProcessActions(EditItemList);
-            }
-            else
-                new MainWindow().Show();
+	        return new List<EditItem>();
         }
 
-        private List<EditItem> ProcessScriptFile(string FileName)
+        private static List<EditItem> ProcessCommandLine(string[] args)
         {
-            return null;
+	        var result = new List<EditItem>();
+
+	        try
+	        {
+		        EditItem item = new EditItem();
+		        item.Parse(args);
+		        result.Add(item);
+	        }
+	        catch (ApplicationException x)
+	        {
+		        MessageBox.Show(x.Message, Title);
+	        }
+
+	        return result;
         }
 
-        private List<EditItem> ProcessCommandLine(string[] args)
-        {
-            return null;
-        }
-
-        void ProcessActions(List<EditItem> EditItemList)
+        private static void ProcessActions(IEnumerable<EditItem> EditItemList)
         {
             foreach (var edit in EditItemList)
             {
+	            string msg = $"I pretend to process this: {edit}";
+	            MessageBox.Show(msg, Title);
                 edit.Execute();
             }
         }
--- EditItem.cs
+++ EditItem.cs
@@ -1,4 +1,6 @@
-﻿namespace PathEdit
+﻿using System;
+
+namespace PathEdit
 {
     public class EditItem
     {
@@ -26,9 +28,41 @@
         private eHive Hive { get; set; }
         private eLocation Location { get; set; }
 
+        public void Parse(string[] args)
+        {
+	        bool success = true;
+
+	        if (Enum.TryParse(args[0], out eAction action))
+		        Action = action;
+	        else success = false;
+
+	        if (Enum.TryParse(args[1], out eHive hive))
+		        Hive = hive;
+	        else success = false;
+
+	        if (Enum.TryParse(args[2], out eLocation location))
+		        Location = location;
+	        else success = false;
+
+	        PathString = args[3];
+
+	        if (success) return;
+
+			throw new ApplicationException("Invalid arguments.");
+        }
+
         public bool Execute()
         {
             return true;
         }
+
+        #region Overrides of Object
+
+        public override string ToString()
+        {
+	        return $"{Action}, {Hive}, {Location}, {PathString}";
+        }
+
+        #endregion
     }
 }
